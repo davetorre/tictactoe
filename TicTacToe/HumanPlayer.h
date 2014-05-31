d2f47b598242b51f4aba482e60601cd927cf3c87
David Torre <davetorre27@gmail.com>
2014-05-31 12:06:07 -0500

    Decouple UI, remove hard-coding in TicTacToeGame

---
+++ TicTacToe/HumanPlayer.h
@@ -0,0 +1,13 @@
+//
+//  HumanPlayer.h
+//  TicTacToe
+//
+//  Created by David Torre on 5/28/14.
+//  Copyright (c) 2014 David Torre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HumanPlayer : NSObject
+
+@end
---
+++ TicTacToe/TicTacToeBoard.h
@@ -0,0 +1,13 @@
+//
+//  TicTacToeBoard.h
+//  TicTacToe
+//
+//  Created by David Torre on 5/30/14.
+//  Copyright (c) 2014 David Torre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TicTacToeBoard : NSObject
+
+@end
